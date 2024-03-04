256cc471c9fb5116ff57010740c3789f5ca32a9b
Gustavo <vasconcelosgu604@gmail.com>
2024-03-04 16:10:00 -0300

    :ambulance: FIxing the fetch route of the user api

---
+++ src/app/api/users/create/route.ts
@@ -0,0 +1,37 @@
+import { NextRequest, NextResponse } from "next/server";
+import User from "@/database/models/users";
+import { hashPassword } from "@/utils/auth";
+import jwt from "jsonwebtoken";
+import "dotenv/config";
+
+export async function POST(request: NextRequest) {
+  const { email, password } = await request.json();
+
+  if (!email || !password) {
+    return NextResponse.json(
+      { message: "Please fill in all fields" },
+      { status: 400 }
+    );
+  }
+
+  const hashedPassword = await hashPassword(password);
+
+  const user = await User.create({ email, password: hashedPassword });
+
+  console.log(user);
+
+  if (!user) {
+    return NextResponse.json(
+      { message: "Invalid credentials" },
+      { status: 401 }
+    );
+  }
+
+  const token = jwt.sign(
+    { email: user.email, id: user.id },
+    process.env.JWT_SECRET!,
+    { expiresIn: "1h" }
+  );
+
+  return NextResponse.json({ token, userId: user.id });
+}
---
+++ src/app/api/users/login/route.ts
@@ -0,0 +1,28 @@
+import { NextRequest, NextResponse } from 'next/server';
+import User from '@/database/models/users';
+import { comparePassword } from '@/utils/auth';
+import jwt from 'jsonwebtoken';
+import 'dotenv/config';
+
+
+export async function POST(request: NextRequest) {
+    const requestBody = await request.json();
+    const { email, password } = requestBody;
+    
+    if (!email || !password) {
+        return NextResponse.json({ message: 'Please fill in all fields' }, { status: 400 });
+    }
+
+    const user = await User.findOne({ where: { email } });
+
+    const isMatch = await comparePassword(password, user!.password);
+    console.log(isMatch);
+
+    if (!isMatch) {
+        return NextResponse.json({ message: 'Invalid credentials' }, { status: 401 });
+    }
+
+    const token = jwt.sign({ email: user!.email, id: user!.id }, process.env.JWT_SECRET!, { expiresIn: '1h' });
+
+    return NextResponse.json({ token, userId: user!.id });
+  }
