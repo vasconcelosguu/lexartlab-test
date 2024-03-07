5d0a0cdf1e67a3ceda0f17c14ef987e302796bae
Gustavo <vasconcelosgu604@gmail.com>
2024-03-07 00:18:49 -0300

    Lista de produtos e homepage

--- src/app/components/ProductsList.tsx
+++ src/app/components/ProductsList.tsx
@@ -24,21 +24,22 @@ export const ProductsList: React.FC = () => {
   }, []);
 
   return (
-    <main className="container mx-auto max-w-md py-8 px-4">
-      <h1 className="text-center text-gray-700 font-medium text-2xl">Products</h1>
-      {loading && <p>Loading...</p>}
-      {!loading && allItems.length === 0 && <p>No products found</p>}
-      <ul>
-        {allItems.map((item) => (
-          <li key={item.id}>
-            <p>{item.name}</p>
-            <p>{item.brand}</p>
-            <p>{item.model}</p>
-            <p>{item.price}</p>
-            <p>{item.color}</p>
-          </li>
-        ))}
-      </ul>
-    </main>
+      <main className="container mx-auto py-8 px-4">
+        <h1 className="text-center text-gray-700 font-medium mb-8 text-2xl">Products</h1>
+        {loading && <p>Loading...</p>}
+        {!loading && allItems.length === 0 && <p>No products found</p>}
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+          {allItems.map((item) => (
+            <div key={item.id} className="border p-4 rounded text-center hover:bg-blue-100 transition-colors duration-200 overflow-auto">
+              <img src="https://via.placeholder.com/150" className="mx-auto block" alt="Product" />
+              <p>Name:{item.name}</p>
+              <p>Brand:{item.brand}</p>
+              <p>Model:{item.model}</p>
+              <p>Price:{item.price}</p>
+              <p>Color{item.color}</p>
+            </div>
+          ))}
+        </div>
+      </main>
   );
 };
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,20 +1,21 @@
-'use client';
+"use client";
 
-import React from 'react';
-import { ProductsList } from '../components/ProductsList';
-import Header from '../components/Header';
+import React from "react";
+import { ProductsList } from "../components/ProductsList";
+import Header from "../components/Header";
 
 const HomePage: React.FC = () => {
-
   return (
     <>
-    <Header />
-        <main className="container mx-auto max-w-md py-8 px-4">
-    <div className="flex justify-center">
-      <h1 className="text-center text-gray-700 font-medium text-2xl">Check our Products Below</h1>
-    </div>
-    <ProductsList />
-  </main>
+      <Header />
+      <main className="container mx-auto py-8 px-4">
+        <section className="flex justify-center">
+          <h1 className="text-center text-gray-700 font-medium text-2xl">
+            Check our Products Below
+          </h1>
+        </section>
+        <ProductsList />
+      </main>
     </>
   );
 };
